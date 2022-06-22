f8e8b87118af887e0fd95b9ad6c83e0b2e06a431
robbo23 <robbo1995@gmail.com>
2022-06-23 00:41:57 +0200

    Update version

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-module.exports = {
-  opcja1: function () {
-    console.log("siemano, testowa paczka");
-  },
-  opcja2: function (t) {
-    console.log("tutaj jest jakas opcja ", t);
-  },
-};
+function oneFunction() {
+  console.log("siemano, testowa paczka");
+}
+
+function twoFunction(t) {
+  console.log("tutaj jest jakas opcja ", t);
+}
+
+module.exports = { oneFunction, twoFunction };
