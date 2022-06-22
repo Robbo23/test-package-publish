29b0e063e50b52fd5b9a1eb47df7697f7b4b21aa
robbo23 <robbo1995@gmail.com>
2022-06-23 00:30:47 +0200

    Update

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-module.exports = (
-
-  console.log('siemano, testowa paczka')  
-  )
+module.exports = {
+  opcja1: function () {
+    console.log("siemano, testowa paczka");
+  },
+  opcja2: function (t) {
+    console.log("tutaj jest jakas opcja ", t);
+  },
+};
